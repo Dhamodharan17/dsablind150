5984b343e2b5c4036bfdc8ad6a6ac1313d9a79bb
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-07-15 16:48:43 +0530

    weightedgraph

---
+++ starters/graphconverters.java
@@ -0,0 +1,28 @@
+class Solution {
+
+
+   static class Pair {
+        int src;
+        int dest;
+        int weight;
+
+        Pair(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+  //type 1 - conversion
+ static int spanningTree(int V, int E, List<List<int[]>> adj) {
+         
+    List<Pair> edges = new ArrayList<>();
+     // extract data  
+        for (int i = 0; i < V; i++) {
+            for (int[] edge : adj.get(i)) {
+                edges.add(new Pair(i, edge[0], edge[1]));
+            }
+        }
+ }
+
+}
